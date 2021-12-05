981a5748427759c827e7752617669ca9715ec481
Tzouvaras Konstantinos <kostastzouva@gmail.com>
2021-12-05 19:13:54 +0200

    remove auxilary code in datamng/views.py

--- datamng/views.py
+++ datamng/views.py
@@ -1,4 +1,3 @@
-import os.path, csv, re, numpy
 import pandas as pd
 from django.shortcuts import render
 from sklearn.model_selection import train_test_split
@@ -15,15 +14,7 @@ def datamng_home(request):
 def runalg(request):
         #read single values from csv
         dataset = open('./dataset.csv', 'r')
-
-        #add lines to a list as separate variables
         content = dataset.read()
-        dataList = re.split(",|\n", content)
-
-        print("\n")
-        print("dataList:", dataList)
-        print("\n")
-
         dataset.close()
 
         #preprocessing
@@ -32,37 +23,17 @@ def runalg(request):
 
         cols = [col for col in dataset.columns if col not in ['addition','subtraction','multiplication','division']]
         y = dataset[cols]
-        y.head(n=2)
-        y = numpy.ravel(y)
-        print("y:", y)
-        print("\n")
 
         cols1 = [col for col in dataset.columns if col not in ['headers', 'parity']]
         X = dataset[cols1]
-        X.head(n=2)
-        print("X:", X)
-        print("\n")
 
         data_train, data_test, target_train, target_test = train_test_split(X, y, test_size = 0.45, random_state = 10)
-
-        print("data_train:", data_train)
-        print("\n")
-        print("target_train:", target_train)
-        print("\n")
-        print("data_test:", data_test)
-        print("\n")
-        print("target_test:", target_test)
-        print("\n")
 
         bayes = GaussianNB() #create an object of the type GaussianNB
 
         prediction = bayes.fit(data_train, target_train).predict(data_test) #train the algorithm on training data and predict using the testing data
-        print("prediction:", prediction.tolist())
-        print("\n")
 
         accuracy = accuracy_score(target_test, prediction, normalize = True)
-        print("Gaussian Naive-Bayes accuracy: ", accuracy) #print the accuracy score of the model
-        print("\n")
 
         bayes.classes_[0]
         visualizer = ClassificationReport(bayes)
