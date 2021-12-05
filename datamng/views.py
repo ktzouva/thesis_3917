80e4182ee8ccee638b0c215fba721cb305199a93
Tzouvaras Konstantinos <kostastzouva@gmail.com>
2021-12-05 21:01:49 +0200

    datamng/views.py optimization

--- datamng/views.py
+++ datamng/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-from yellowbrick.classifier import ClassificationReport
+from yellowbrick.classifier import classification_report
 
 def datamng_home(request):
     if request.user.is_authenticated:
@@ -21,13 +21,13 @@ def runalg(request):
         headers = ["addition","subtraction","multiplication","division", "parity"]
         dataset = pd.read_csv('./dataset.csv', names=headers)
 
-        cols = [col for col in dataset.columns if col not in ['addition','subtraction','multiplication','division']]
-        y = dataset[cols]
+        parityCol = [col for col in dataset.columns if col not in ['addition','subtraction','multiplication','division']]
+        y = dataset[parityCol]
 
-        cols1 = [col for col in dataset.columns if col not in ['headers', 'parity']]
-        X = dataset[cols1]
+        dataCols = [col for col in dataset.columns if col not in ['headers', 'parity']]
+        X = dataset[dataCols]
 
-        data_train, data_test, target_train, target_test = train_test_split(X, y, test_size = 0.45, random_state = 10)
+        data_train, data_test, target_train, target_test = train_test_split(X, y, random_state = 10)
 
         bayes = GaussianNB() #create an object of the type GaussianNB
 
@@ -35,16 +35,12 @@ def runalg(request):
 
         accuracy = accuracy_score(target_test, prediction, normalize = True)
 
-        bayes.classes_[0]
-        visualizer = ClassificationReport(bayes)
-        visualizer.fit(data_train, target_train) # Fit the training data to the visualizer
-        visualizer.score(data_test, target_test) # Evaluate the model on the test data
-        g = visualizer.poof()
+        classification_report(bayes, data_train, target_train, data_test, target_test, classes = dataCols)
 
         list_content = content.split()
         list_datatrain = data_train.values.tolist()
+        list_targettrain = target_train.values.tolist()
         list_datatest = data_test.values.tolist()
+        list_targettest = target_test.values.tolist()
 
-        args = {'content': list_content, 'data_train': list_datatrain, 'target_train': target_train, 'data_test': list_datatest, 'target_test': target_test, 'prediction': prediction, 'accuracy': accuracy}
-
-        return render(request, 'datamng/datamng_runalg.html', args)
+        return render(request, 'datamng/datamng_runalg.html', {'content': list_content, 'data_train': list_datatrain, 'target_train': list_targettrain, 'data_test': list_datatest, 'target_test': list_targettest, 'prediction': prediction, 'accuracy': accuracy})
